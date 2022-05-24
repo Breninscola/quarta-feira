479f157692b00f827a6ab12141e7de53e77bb594
xBr3no <101678178+xBr3no@users.noreply.github.com>
2022-05-24 20:48:27 -0300

    Add files via upload

---
+++ amanhaecuartaindex.js
@@ -0,0 +1,11 @@
+console.log("Oi eu sou Breno e amanha e quarta e depois quinta e finalmente sextaaaaaaa aeeeee");
+var firstname="Brenoo";
+var lastname="Prado Pereira";
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+var a = 2;
+var b = 5;
+var c = a+b;
+console.log(c)
+var raiz = Math.sqrt(81);
+console.log(raiz);
